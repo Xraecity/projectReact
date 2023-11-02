e753c8dd188b5eb6a6a6cdc0193d4d126a013906
Prince Xrae <49319398+Xraecity@users.noreply.github.com>
2023-11-01 21:03:24 -0500

    Update Random.js

--- src/component/Random.js
+++ src/component/Random.js
@@ -1,21 +1,26 @@
+/* Name: Prince Obioha
+  Date: 30th October, 2023
+*/
+
 import React, { useState, useEffect } from "react";
 import "../styles.css";
-import "mvp.css";
 
 function Random() {
   const [apiEntry, setApiEntry] = useState(null);
   const [isHighlighted, setIsHighlighted] = useState(false);
-  const [buttonColor, setButtonColor] = useState("black"); //defualt colicr
+  const [buttonColor, setButtonColor] = useState("black");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredCategories, setFilteredCategories] = useState([]);
+  const [searchButtonColor, setSearchButtonColor] = useState("black"); 
 
-  const handleButtonClick = () => {
+  const clearData = () => {
+    setApiEntry(null);
+  };
+
+  const fetchRandomData = () => {
     setIsHighlighted(false);
+    setButtonColor("dodgerblue");
 
-    // Toggle button color when clicked
-    setButtonColor((prevColor) =>
-      prevColor === "orange" ? "dodgerblue" : "orange"
-    );
-
-    // fetching the ApI given
     fetch("https://api.publicapis.org/random")
       .then((response) => response.json())
       .then((data) => {
@@ -23,51 +28,138 @@ function Random() {
       });
   };
 
-  // apply the api
-  useEffect(() => {
-    fetch("https://api.publicapis.org/random")
+  const fetchByCategory = (category) => {
+    setIsHighlighted(false);
+    setButtonColor("orange");
+
+    fetch(`https://api.publicapis.org/random?category=${category}`)
       .then((response) => response.json())
       .then((data) => {
         setApiEntry(data.entries[0]);
       });
+  };
+
+  useEffect(() => {
+    fetchRandomData();
   }, []);
 
-  if (!apiEntry) {
-    // Adding Preloading :)
-    return <div>Loading...</div>;
-  }
+  const apiCategories = [
+    "Business",
+    "Science & Math",
+    "Health",
+    "Music",
+    "Weather",
+    "Technology",
+    "Food & Drink",
+    "Animals",
+    "Sports & Fitness",
+    "Books",
+    "Games",
+  ];
+
+  const handleSearch = (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    setSearchTerm(searchTerm);
+
+    const filtered = apiCategories.filter((category) =>
+      category.toLowerCase().includes(searchTerm)
+    );
+    setFilteredCategories(filtered);
+    setSearchButtonColor("black"); // Set the search button color
+  };
+
+  
+  const handleButtonClick = () => {
+    setIsHighlighted(false);
+
+    // Toggle button color between orange and dodgerblue when clicked
+    setButtonColor((prevColor) =>
+      prevColor === "orange" ? "dodgerblue" : "orange"
+    );
+  };
+
+  const handleCategoryButtonClick = (category) => {
+    setIsHighlighted(false);
+
+    setButtonColor((prevColor) =>
+      prevColor === "orange" ? "dodgerblue" : "orange"
+    );
+
+    fetchByCategory(category);
+  };
 
   return (
     <div className="card">
-      <h1>Assignment 5: Random API Entry</h1>
+      <h1 style={{ color: "#333", fontSize: "24px" }}>Random API Entry</h1>
       <p>
-        <b>Name: </b>
-        <span className={isHighlighted ? "button-highlighted" : ""}>
-          {apiEntry.API}
+        <b>API Content:</b>{" "}
+        <span
+          className={isHighlighted ? "button-highlighted" : ""}
+          style={{ fontSize: "18px", fontWeight: "bold" }}
+        >
+          {apiEntry && apiEntry.API}
+        </span>
+      </p>
+      <p>
+        <b>Category:</b>{" "}
+        <span style={{ fontSize: "16px" }}>
+          {apiEntry ? apiEntry.Category : "No category"}
         </span>
       </p>
       <i>
         <p>
-          <b>Description:</b> {apiEntry.Description}
+          <b>Description:</b>{" "}
+          <span style={{ fontSize: "16px" }}>
+            {apiEntry ? apiEntry.Description : "No description"}
+          </span>
         </p>
       </i>
       <br />
-      <br />
 
-      <center>
-        <button
-          // Style the btn
-          style={{
-            backgroundColor: buttonColor,
-            color: "white",
-            outline: "3px ridge white",
-            borderRadius: "20% 0% 20% 0%"
-          }}
-          onClick={handleButtonClick}
-        >
-          Generate New API Name
+      <input
+        type="text"
+        placeholder="Search for API Categories here"
+        className="search-input"
+        onChange={handleSearch}
+        value={searchTerm}
+      />
+
+      <div className="button-container">
+        <button className="clear-button" onClick={clearData}>
+          Clear Data
         </button>
-      </center>
+        {searchTerm
+          ? filteredCategories.map((category, index) => (
+              <button
+                className="custom-button"
+                key={index}
+                onClick={() => handleCategoryButtonClick(category)}
+                style={{
+                  backgroundColor: buttonColor,
+                  color: "white",
+                  outline: "3px ridge white",
+                  borderRadius: "20% 0% 20% 0%"
+                }}
+              >
+                {category}
+              </button>
+            ))
+          : apiCategories.map((category, index) => (
+              <button
+                className="custom-button"
+                key={index}
+                onClick={() => handleCategoryButtonClick(category)}
+                style={{
+                  backgroundColor: buttonColor,
+                  color: "white",
+                  outline: "3px ridge white",
+                  borderRadius: "20% 0% 20% 0%"
+                }}
+              >
+                {category}
+              </button>
+            ))}
+      </div>
     </div>
   );
 }
